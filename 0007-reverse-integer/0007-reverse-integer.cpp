82918d434cb726c5c42047a41515a318b043121c
Satya Narayan <163873267+DCodeLIFE@users.noreply.github.com>
2026-07-19 14:24:08 +0530

    Time: 5 ms (20.17%), Space: 8.4 MB (98.29%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int n=0,y;
+        while (x!=0)
+        {
+            y=x%10;
+            if (n > INT_MAX / 10 || (n == INT_MAX / 10 && y > 7)) {
+                return 0;
+            }
+            if (n < INT_MIN / 10 || (n == INT_MIN / 10 && y < -8)) {
+                return 0;
+            }
+            n=n*10+y;
+            x=x/10;
+        }
+        x=n;
+        return(x);
+        
+    }
+};
